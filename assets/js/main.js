9e4b9a689596b93f9c6ff19ccc1d9e14b0ac573d
leah nu nguyen <leahnunguyen@leahs-MacBook-Air.local>
2026-03-25 19:54:38 -0500

    adding content and changing layouts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
-// main.js
+(function () {
+	var nodes = document.querySelectorAll(".word-node");
+	if (!nodes.length || window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+		return;
+	}
 
-console.log("Hello, World!");
+	var ticking = false;
+	var lastY = 0;
+
+	function applyParallax() {
+		var y = window.scrollY || window.pageYOffset;
+		lastY = y;
+		nodes.forEach(function (el) {
+			var rate = parseFloat(el.getAttribute("data-parallax"));
+			if (isNaN(rate)) rate = 0.12;
+			var offset = y * rate * -0.45;
+			el.style.setProperty("--parallax-y", offset.toFixed(2) + "px");
+		});
+		ticking = false;
+	}
+
+	function onScroll() {
+		if (!ticking) {
+			ticking = true;
+			requestAnimationFrame(applyParallax);
+		}
+	}
+
+	window.addEventListener("scroll", onScroll, { passive: true });
+	applyParallax();
+})();
